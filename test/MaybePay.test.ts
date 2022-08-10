6c5669bfdc2f2235042d02b8385e74eb3651316c
Andrew Morris <voltrevo@gmail.com>
2022-08-10 11:49:40 +1000

    Add failing test

--- test/MaybePay.test.ts
+++ test/MaybePay.test.ts
@@ -51,4 +51,67 @@ describe('MaybePay', () => {
 
     await fx.expectApproxEth(fx.consumer.address, 10000);
   });
+
+  // eslint-disable-next-line max-len
+  it("should allow service provider to claim consumer's funds with correct message", async () => {
+    const fx = await Fixture();
+
+    await (
+      await fx.consumer.sendTransaction({
+        to: fx.maybePay.address,
+        value: ethers.utils.parseEther('1.0'),
+      })
+    ).wait();
+
+    expect(
+      ethers.utils.formatEther(
+        await fx.consumerToMaybePay.balances(fx.consumer.address),
+      ),
+    ).to.eq('1.0');
+
+    const amount = ethers.utils.parseEther('0.1');
+    const serverSecret = 5;
+    const consumerMixer = 2;
+    const threshold = 10;
+
+    const serverSecretHash = ethers.utils.solidityKeccak256(
+      ['uint'],
+      [serverSecret],
+    );
+
+    const consumerSignature = await fx.consumer.signMessage(
+      ethers.utils.solidityKeccak256(
+        ['uint', 'address', 'uint', 'address', 'uint', 'uint', 'uint'],
+        [
+          ethers.provider.network.chainId,
+          fx.maybePay.address,
+          amount,
+          fx.serviceProvider.address,
+          serverSecretHash,
+          consumerMixer,
+          threshold,
+        ],
+      ),
+    );
+
+    await (
+      await fx.serviceProviderToMaybePay.claim(
+        amount,
+        fx.consumer.address,
+        serverSecret,
+        consumerMixer,
+        threshold,
+        consumerSignature,
+      )
+    ).wait();
+
+    expect(
+      ethers.utils.formatEther(
+        await fx.consumerToMaybePay.balances(fx.consumer.address),
+      ),
+    ).to.eq('0.9'); // Down from 1.0
+
+    // Up from 10000.0
+    await fx.expectApproxEth(fx.serviceProvider.address, 10000.1);
+  });
 });
